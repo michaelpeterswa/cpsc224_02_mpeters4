651836cf1f78dfd748e55b7d260b40569c099970
Michael Peters <michaelpeterswa@icloud.com>
2019-01-22 13:31:59 -0800

    Uses Swing for dialog boxes now

--- hw1/src/Yahtzee.java
+++ hw1/src/Yahtzee.java
@@ -7,14 +7,20 @@ Date: 01.20.19
 
 import java.util.Scanner;
 import java.util.Random;
+import javax.swing.JOptionPane;
 
 public class Yahtzee {
     public static void main(String[] args) {
-        System.out.println("Welcome to Yahtzee!");
-        inputName();
+        int[] rolls = new int[5];
+
+        //inputName();
         for (int i = 0; i < 5; i++){
-            System.out.println(rollDie());
+            rolls[i] = rollDie();
         }
+
+        String message = "Roll 1: " +rolls[0] + "  Roll 2: " + rolls[1] + "  Roll 3: " + rolls[2] + "  Roll 4: " + rolls[3] + "  Roll 5: " + rolls[4];
+        JOptionPane.showMessageDialog(null, message, "Current Dice Roll", JOptionPane.PLAIN_MESSAGE);
+        System.exit(0);
     }
     private static void inputName(){
         Scanner s = new Scanner(System.in);

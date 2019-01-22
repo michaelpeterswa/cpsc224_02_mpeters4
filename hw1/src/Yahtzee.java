3434d7ad58821c7c5f735bc3211958bbb7b4a2cc
Michael Peters <michaelpeterswa@icloud.com>
2019-01-22 12:05:36 -0800

    Updated For HW1

---
+++ hw1/src/Yahtzee.java
@@ -0,0 +1,12 @@
+/*
+Michael Peters
+HW1
+Partners:
+Date: 01.20.19
+ */
+
+public class Yahtzee {
+    public static void main(String[] args) {
+        System.out.println("Hello World!");
+    }
+}

5173060cc72f86263921f15aeb8c50503f04c998
Michael Peters <michaelpeterswa@icloud.com>
2019-02-11 19:50:32 -0800

    added menu choice, added word choice, added continuous interaction.

--- hw2/src/Hangman.java
+++ hw2/src/Hangman.java
@@ -1,28 +1,38 @@
-import java.util.Scanner;
 import javax.swing.JOptionPane;
 import java.util.Random;
-import java.util.*;
 
 public class Hangman {
     public static void main(String[] args) {
         //inputName();
 
-        //String message = "fuck you bih";
         //JOptionPane.showMessageDialog(null, message, "Hello!", JOptionPane.WARNING_MESSAGE);
         gameTime();
     }
 
-    private static void inputName() {
-        Scanner s = new Scanner(System.in);
-        System.out.print("Enter your name: ");
-        String name = s.nextLine();
-        System.out.println("Hello " + name + "!");
+    private static String inputWord() {
+        String j;
+        j = JOptionPane.showInputDialog(null, "Enter a word");
+        return j.toLowerCase();
+
 
     }
+    private static char inputMenu() {
+        String j;
+        j = JOptionPane.showInputDialog(null, "Would you like to play hangman? (y/n)");
+        return j.toLowerCase().charAt(0);
+    }
 
-    public static String randWords() {
-        String hangWords [] = {"help", "awkward" ,"bagpipes" , "banjo" , "bungler", "croquet", "crypt", "dwarves" ,"fervid", 
-        "fishhook","fjord", "gypsy",  "haiku", "haphazard", "jinx", "kiosk", "memento", "ostracize", "phlegm", 
+
+    private static char inputChoice() {
+        String j;
+        j = JOptionPane.showInputDialog(null, "Would you like to use your own word? (y/n): ");
+        return j.toLowerCase().charAt(0);
+    }
+
+    private static String randWords() {
+        String [] hangWords;
+        hangWords = new String[]{"help", "awkward" ,"bagpipes" , "banjo" , "bungler", "croquet", "crypt", "dwarves" ,"fervid",
+        "fishhook","fjord", "gypsy",  "haiku", "haphazard", "jinx", "kiosk", "memento", "ostracize", "phlegm",
         "pixel", "rogue", "sphinx", "waxy", "wildebeest", "yacht", "zealous" };
 
         Random rand = new Random();
@@ -30,73 +40,84 @@ public class Hangman {
         return hangWords[i];
     }
     
-    public static void gameTime() {
-        String word = randWords();
-        int amountOfMisses;
-        amountOfMisses = 0;
-        int length = word.length();
-        String dashes = "";
-        for (int i = 0; i < length; i++)
-        {
-            dashes = dashes + "_";
-        }
-        String placeHolder = dashes;
+    private static void gameTime() {
+        String word;
+        char menu = inputMenu();
+        if(menu == 'y') {
+            char choice = inputChoice();
+            if (choice == 'n') {
+                word = randWords();
+            } else {
+                word = inputWord();
+            }
+            int amountOfMisses;
+            amountOfMisses = 0;
+            int length = word.length();
+            String dashes = "";
+            for (int i = 0; i < length; i++) {
+                dashes = dashes + "_";
+            }
+            String placeHolder = dashes;
 
-        boolean win = true;
+            boolean win = true;
 
-        while (!word.equals(dashes)){
-          dashes = correctGuess(word, dashes, length);
-          if (dashes == placeHolder) {
-            amountOfMisses++;
-            theHangedMan(amountOfMisses);
-            if(amountOfMisses > 5) {
-                win = false;
-                break;}
-          }
-          else
-          {
-              placeHolder = dashes;
-          }
-        }
-        
-        if(win == true)
-        {
-            JOptionPane.showMessageDialog(null, "Congratulations    \n your word was:   \n  " + word);
-        }
-        else
-            JOptionPane.showMessageDialog(null, "**Hangman** \n************** \n----------- \n  " + word + "\n----------- \n**************");
-    } 
+            while (!word.equals(dashes)) {
+                dashes = correctGuess(word, dashes, length);
+                if (dashes.equals(placeHolder)) {
+                    amountOfMisses++;
+                    theHangedMan(amountOfMisses);
+                    if (amountOfMisses > 5) {
+                        win = false;
+                        break;
+                    }
+                } else {
+                    placeHolder = dashes;
+                }
+            }
 
-    public static String correctGuess (String word, String dashes, int length) {
+            if (win) {
+                JOptionPane.showMessageDialog(null, "Congratulations    \n your word was:   \n  " + word);
+                gameTime();
+            } else
+                JOptionPane.showMessageDialog(null, "**Hangman** \n************** \n----------- \n  " + word + "\n----------- \n**************");
+                gameTime();
+        }
+        else System.exit(0);
+        }
+
+    private static String correctGuess(String word, String dashes, int length) {
         
         JOptionPane.showMessageDialog(null, "**Hangman** \n************** \n----------- \n  " + dashes + "\n----------- \n**************");
         String j;
         boolean flag = false;
-        j = JOptionPane.showInputDialog(null, "Enter a letter");
-        j.toLowerCase();
-        char guess = j.charAt(0);
-        if (Character.isLetter(guess))
-        {
-          for (int i = 0; i < length; i++) 
-          {
-              if (guess == word.charAt(i)) {
-                dashes = changeCharInPosition(i, guess, dashes);
-                flag = true; }
-          }
+        j = JOptionPane.showInputDialog(null, "Enter a letter").toLowerCase();
+        if(!j.isEmpty()) {
+            char guess = j.charAt(0);
+            if (Character.isLetter(guess)) {
+                for (int i = 0; i < length; i++) {
+                    if (guess == word.charAt(i)) {
+                        dashes = changeCharInPosition(i, guess, dashes);
+                        flag = true;
+                    }
+                }
+            }
         }
-        if (flag == false) {
+        else {
+            correctGuess(word, dashes, length);
+        }
+        if (!flag) {
             JOptionPane.showMessageDialog(null, "You missed");
         }
         return dashes;
     }
 
-    public static String changeCharInPosition(int position, char ch, String str) {
+    private static String changeCharInPosition(int position, char ch, String str) {
         char[] charArray = str.toCharArray();
         charArray[position] = ch;
         return new String(charArray);
     }
 
-    public static void theHangedMan(int pain) {
+    private static void theHangedMan(int pain) {
         if (pain == 1) 
             JOptionPane.showMessageDialog(null, "   O   ");
         if (pain == 2) 
